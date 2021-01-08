8a7a3141142e17d474f01de5ed56ae15d935cee9
JeffreyCA <jeffreyca16@gmail.com>
2021-01-07 21:52:54 -0500

    Reduce SVG size and fix styling issues

--- components/TrackList/TrackListHeader.tsx
+++ components/TrackList/TrackListHeader.tsx
@@ -1,4 +1,4 @@
-import { Image, Typography } from 'antd';
+import { Image, Space, Typography } from 'antd';
 import LastFmIcon from '../../public/lastfm.svg';
 
 const { Text } = Typography;
@@ -15,11 +15,17 @@ interface Props {
  */
 export default function TrackListHeader(props: Props): JSX.Element {
     return (
-        <span>
-            <a target="_blank" rel="noopener noreferrer" href={`https://www.last.fm/user/${props.username}`}>
-                <Image className="lastfm-icon" src={LastFmIcon} width={60}></Image>
-            </a>
-            <Text className="lastfm-title">Recently Played</Text>
-        </span>
+        <div style={{ display: 'flex' }}>
+            <Space>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`https://www.last.fm/user/${props.username}`}
+                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: -1 }}>
+                    <Image preview={false} className="lastfm-icon" src={LastFmIcon} width={60}></Image>
+                </a>
+                <Text className="lastfm-title">Recently Played</Text>
+            </Space>
+        </div>
     );
 }
